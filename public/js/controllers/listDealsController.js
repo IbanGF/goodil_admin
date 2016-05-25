b7196f54b3e5109561fc19e273515d26427e4204
IbanGF <ueqiban@gmail.com>
2016-05-25 09:42:35 +0200

    GOODIL update almost done backend

--- public/js/controllers/listDealsController.js
+++ public/js/controllers/listDealsController.js
@@ -1,25 +1,29 @@
 // AddDeal CONTROLLER
-function listDealsController($scope, $http, dealsService) {
+function listDealsController($scope, $http, dealsService, categoriesService) {
   $scope.title = "List Deals";
 
   function load() {
     dealsService.getDeals().then(function(res) {
       var deals = res.data;
 
-      console.log(deals);
-      var categories = [];
-      var subCategories = [];
-      var unique = {};
-      for (var i in deals) {
-        if (typeof(unique[deals[i].subCategory.name]) == "undefined") {
-          subCategories.push(deals[i].subCategory.name);
-        }
-        unique[deals[i].subCategory.name] = 0;
-      }
+      // console.log(deals);
+      // var categories = [];
+      // var subCategories = [];
+      // var unique = {};
+      // for (var i in deals) {
+      //   if (typeof(unique[deals[i].subCategory.name]) == "undefined") {
+      //     subCategories.push(deals[i].subCategory.name);
+      //   }
+      //   unique[deals[i].subCategory.name] = 0;
+      // }
 
       console.log(subCategories);
     });
   }
+
+  categoriesService.getCategories().then(function(res) {
+    $scope.categories = res.data;
+  });
 
   load();
 
