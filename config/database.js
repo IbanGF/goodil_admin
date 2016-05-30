f531b117872ea9a8e02382d407b81384418cc188
BuildTools <unconfigured@null.spigotmc.org>
2016-05-31 00:13:47 +0200

    prod V1

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 // DATABASE CONFIG
 var mongoose = require('mongoose');
-module.exports = mongoose.connect(process.env.MONGODB_URI);
+module.exports = mongoose.connect('mongodb://localhost/goodil_admin');
