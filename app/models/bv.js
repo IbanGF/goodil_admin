0da80dbeaf2b729842d504a2115dbf3630b05091
IbanGF <ueqiban@gmail.com>
2016-06-25 22:58:00 +0200

    Final countdown

--- app/models/bv.js
+++ app/models/bv.js
@@ -28,34 +28,6 @@ var BassinDeVie = {
 
   model: mongoose.model('BassinDeVie', BassinDeVieCollection),
 
-  /*addBVFile: function (req, res) {
-      fs.writeFile('public/bv/bv.csv', req.body.fileContent, "utf8", function (err) {
-          if (err) throw err;
-          console.log(req.body.fileName + ' saved! in : public/bv/' + req.body.fileName);
-          var readStream = fs.createReadStream('public/bv/bv.csv');
-          var writeStream = fs.createWriteStream('public/bv/bv.json');
-          csvConverter.on("end_parsed", function () {
-              res.sendStatus(200); //here is your result json object
-          });
-          readStream.pipe(csvConverter).pipe(writeStream);
-      });
-  },*/
-
-  /*geocodeCities: function (req, res) {
-      BV.model.find({}, 'LIBGEO', function (err, data) {
-          var cities = [];
-          var obj = JSON.parse(JSON.stringify(data));
-          for (var i = 0; i < obj.length; i++) {
-              console.log(obj[i].LIBGEO + ", France");
-              cities.push(obj[i].LIBGEO + ", France");
-          }
-          geocodio.post('geocode', cities.slice(0, 101), function (err, response) {
-              if (err) throw err;
-              res.send(response);
-          });
-      });
-  },*/
-
   findAllBVs: function(req, res) {
     var perPage = 100,
       page = Math.max(0, req.param('page'));
@@ -71,30 +43,6 @@ var BassinDeVie = {
 
 
   findAllBVsName: function(req, res) {
-
-    var pipeline = [{
-      $project: {
-        _id: 0,
-        BVName: 1,
-        BVCode: 1
-      }
-    }, {
-      $group: {
-        _id: "$BVCode",
-        name: "$BVName"
-      }
-    }];
-
-
-    // .find()
-    // .distinct('BVName')
-    // .aggregate(pipeline)
-    // .exec(function(err, bvs) {
-    //   console.log(bvs);
-    //   res.send(bvs);
-    // });
-
-
     BassinDeVie.model.aggregate([{
       $project: {
         _id: 0,
@@ -119,6 +67,21 @@ var BassinDeVie = {
     console.log('cp :' + req.params.codePostal);
     BassinDeVie.model.findOne({
         CodePostal: Number(req.params.codePostal)
+      },
+      function(err, data) {
+        if (err) {
+          console.log(err);
+          res.send(err);
+        } else {
+          console.log('data : ' + data);
+          res.send(data);
+        }
+      });
+  },
+
+  findAllCitiesByBVCode: function(req, res) {
+    BassinDeVie.model.find({
+        BVCode: Number(req.params.BVCode)
       },
       function(err, data) {
         if (err) {
--- app/routes/bv.js
+++ app/routes/bv.js
@@ -2,5 +2,6 @@ var BassinDeVie = require('../models/bv.js');
 module.exports = function (app) {
     app.get('/findAllBVs/:page', BassinDeVie.findAllBVs);
     app.get('/findAllBVsName', BassinDeVie.findAllBVsName);
+    app.get('/findAllCitiesByBVCode/:BVCode', BassinDeVie.findAllCitiesByBVCode);
     app.get('/findOneBV/:codePostal', BassinDeVie.findOneBV);
 };
