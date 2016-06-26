15f6c550c18197817741b79e1d0ba47e76395c28
IbanGF <ueqiban@gmail.com>
2016-06-26 14:10:48 +0200

    Final countdown

--- app/routes/shops.js
+++ app/routes/shops.js
@@ -2,7 +2,7 @@ var Shop = require('../models/shops.js');
 var Auth = require('../middlewares/authorization.js');
 module.exports = function (app) {
     app.get('/shop', Shop.findAllShops);
-    app.get('/shop/:BVCode', Shop.findAllDealsInBV);
+    app.get('/shop/:BVCode', Shop.findAllShopsInBV);
     app.get('/shop/:id', Shop.findOneShop);
     app.post('/shop/uploadShopImage', Auth.user.isAdministrator, Shop.uploadShopImage);
     app.post('/shop', Auth.user.isAdministrator, Shop.createShop);
